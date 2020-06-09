51582c21fbbd2dfb4e1c57d49ab2a45bdb42b953
ginoarpesella@gmail.com <ginoarpesella+tellermate@gmail.com>
2020-06-09 15:43:54 +0200

    Removed hard coded path to files

--- Form1.cs
+++ Form1.cs
@@ -11,68 +11,68 @@ using System.Windows.Forms;
 
 namespace RecipeManager
 {
-    public partial class Form1 : Form
+  public partial class Form1 : Form
+  {
+    private List<Recipe> m_recipes = new List<Recipe>();
+
+    public Form1()
     {
-        private List<Recipe> m_recipes = new List<Recipe>(); 
+      InitializeComponent();
 
-        public Form1()
-        {
-            InitializeComponent();
-
-            LoadRecipes();
-        }
-
-        private void LoadRecipes()
-        {
-            DirectoryInfo directoryInfo = new DirectoryInfo(@"e:\portkata");
-            m_recipes = directoryInfo.GetFiles("*")
-                .Select(fileInfo => new Recipe { Name = fileInfo.Name, Size = fileInfo.Length, Text = File.ReadAllText(fileInfo.FullName) }).ToList();
-
-            PopulateList();
-        }
-
-        private void PopulateList()
-        {
-            listView1.Items.Clear();
-
-            foreach (Recipe recipe in m_recipes)
-            {
-                listView1.Items.Add(new RecipeListViewItem(recipe));
-            }
-        }
-
-        private void DeleteClick(object sender, EventArgs e)
-        {
-            foreach (RecipeListViewItem recipeListViewItem in listView1.SelectedItems)
-            {
-                m_recipes.Remove(recipeListViewItem.Recipe);
-                File.Delete(@"e:\portkata\" + recipeListViewItem.Recipe.Name);
-            }
-            PopulateList();
-
-            NewClick(null, null);
-        } 
-
-        private void NewClick(object sender, EventArgs e)
-        {
-            textBoxName.Text = "";
-            textBoxObjectData.Text = "";
-        }
-
-        private void SaveClick(object sender, EventArgs e)
-        {
-            File.WriteAllText(Path.Combine("e:\\portkata", textBoxName.Text), textBoxObjectData.Text);
-            LoadRecipes();
-        }
-
-        private void SelectedIndexChanged(object sender, EventArgs e)
-        {
-            foreach (RecipeListViewItem recipeListViewItem in listView1.SelectedItems)
-            {
-                textBoxName.Text = recipeListViewItem.Recipe.Name;
-                textBoxObjectData.Text = recipeListViewItem.Recipe.Text;
-                break;
-            }
-        }
+      LoadRecipes();
     }
+
+    private void LoadRecipes()
+    {
+      DirectoryInfo directoryInfo = new DirectoryInfo(Directory.GetCurrentDirectory());
+      m_recipes = directoryInfo.GetFiles("*")
+          .Select(fileInfo => new Recipe { Name = fileInfo.Name, Size = fileInfo.Length, Text = File.ReadAllText(fileInfo.FullName) }).ToList();
+
+      PopulateList();
+    }
+
+    private void PopulateList()
+    {
+      listView1.Items.Clear();
+
+      foreach (Recipe recipe in m_recipes)
+      {
+        listView1.Items.Add(new RecipeListViewItem(recipe));
+      }
+    }
+
+    private void DeleteClick(object sender, EventArgs e)
+    {
+      foreach (RecipeListViewItem recipeListViewItem in listView1.SelectedItems)
+      {
+        m_recipes.Remove(recipeListViewItem.Recipe);
+        File.Delete(@"e:\portkata\" + recipeListViewItem.Recipe.Name);
+      }
+      PopulateList();
+
+      NewClick(null, null);
+    }
+
+    private void NewClick(object sender, EventArgs e)
+    {
+      textBoxName.Text = "";
+      textBoxObjectData.Text = "";
+    }
+
+    private void SaveClick(object sender, EventArgs e)
+    {
+      File.WriteAllText(Path.Combine("e:\\portkata", textBoxName.Text), textBoxObjectData.Text);
+      LoadRecipes();
+    }
+
+    private void SelectedIndexChanged(object sender, EventArgs e)
+    {
+      foreach (RecipeListViewItem recipeListViewItem in listView1.SelectedItems)
+      {
+        textBoxName.Text = recipeListViewItem.Recipe.Name;
+        textBoxObjectData.Text = recipeListViewItem.Recipe.Text;
+        break;
+      }
+    }
+  }
 }
